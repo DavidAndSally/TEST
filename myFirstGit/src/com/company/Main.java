e5140c897d1a2e886c7617c07ea64c9abeaae447
qiwei11 <qiwei11@jd.com>
2017-11-05 18:18:35 +0800

    first commit

---
+++ myFirstGit/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("获取数组args[]的数据");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
